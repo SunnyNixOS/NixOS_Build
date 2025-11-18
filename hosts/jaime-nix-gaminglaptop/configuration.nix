943e75c512f85586d53b828f2ccf0b80273ad16e
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-11-17 23:35:39 -0800

    laptop wireless internet

--- hosts/jaime-nix-gaminglaptop/configuration.nix
+++ hosts/jaime-nix-gaminglaptop/configuration.nix
@@ -21,7 +21,7 @@ in {
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "jaime-nix-gaminglaptop"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
