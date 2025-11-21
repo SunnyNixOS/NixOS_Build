ec01bab9a3ebef85140140486984f7e7ae3efe8c
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-11-20 22:45:18 -0800

    nerdfonts

--- home/hyprland/home.nix
+++ home/hyprland/home.nix
@@ -9,6 +9,7 @@
       ./hyprland.nix
       ./catppuccin.nix
       ./hyprpanel.nix
+      ./fonts.nix
     ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
